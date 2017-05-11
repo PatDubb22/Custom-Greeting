ead284e429accc88e4595319bf2129bac42b52ec
Patricia Wheeler <pgwheeler@cox.net>
2017-05-11 00:25:03 +0000

    GREETINGS

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php   
+    
+        $name = "Patricia";
+        $name = $_GET ["name"];
+        $string="PDUBB";
+        $string="whatever";
+        
+            if ( $name =="Patricia") {
+                echo "Welcome back Patricia! We're excited to get started";
+                
+            }
+            elseif ($string == "PDUBB") {
+            
+               echo  "Hey PDUBB!";
+            }
+            else ($string== "whatever");
+            {
+                echo "Hi, What is your name?";
+            } 
+    ?>
